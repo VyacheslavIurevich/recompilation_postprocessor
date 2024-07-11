b30221b44f37bcbd76f23a8eba32874a7ec463cf
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2024-07-11 23:19:52 +0300

    feat: addition of function PLT checker
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

---
+++ src/function_is_plt.py
@@ -0,0 +1,26 @@
+'''Check if input function is PLT jump'''
+
+#pylint: disable=wrong-import-order
+import pyhidra # pylint: disable=import-error
+from elftools.elf.elffile import ELFFile # pylint: disable=import-error
+pyhidra.start()
+
+def get_got_bounds(path):
+    '''Get GOT section addresses bounds'''
+    elffile = ELFFile(path)
+    section = elffile.get_section_by_name('.got')
+    return section.header.sh_addr, section.header.sh_addr + section.header.sh_size - 2
+
+def function_is_plt(function, program, path):
+    '''Check if input function is PLT jump'''
+    listing = program.getListing()
+    body = function.getBody()
+    for address in body.getAddresses(True):
+        code_unit = str(listing.getCodeUnitAt(address))
+        if code_unit.startswith("JMP qword ptr"):
+            words = code_unit.split()
+            address_str = words[-1][1:-1] # removing []
+            address = int(address_str, 16)
+            got_start, got_end = get_got_bounds(path)
+            return got_start <= address - 16 ** 5 <= got_end
+    return False

b8e9a819733430809c4e13868fb83bd0c1f0f2ba
Sarapulov Vasilii <sarapulov.vas@mail.ru>
2024-08-01 15:05:28 +0300

    fix: pyling warning fix
    Signed-off-by: Sarapulov Vasilii <sarapulov.vas@mail.ru>

--- src/tests/user_tests.py
+++ src/tests/user_tests.py
@@ -24,25 +24,22 @@ def compile_binary(binary):
     return os.system(f"{COMPILER} {OUTPUT_PATH}")
 
 
-class TestUser:
-    """User scenario tests"""
+def test_hello_world(clean):
+    """Recompiles hello world binary"""
+    assert compile_binary("hello_world") == 0
 
-    def test_hello_world(self, clean):
-        """Recompiles hello world binary"""
-        assert compile_binary("hello_world") == 0
+def test_bmp(clean):
+    """Recompiles BMP header reader binary"""
+    assert compile_binary("bmp1") == 0
 
-    def test_bmp(self, clean):
-        """Recompiles BMP header reader binary"""
-        assert compile_binary("bmp1") == 0
+def test_bst(clean):
+    """Recompiles binary search tree binary"""
+    assert compile_binary("bst") == 0
 
-    def test_bst(self, clean):
-        """Recompiles binary search tree binary"""
-        assert compile_binary("bst") == 0
+def test_avl(clean):
+    """Recompiles AVL tree binary"""
+    assert compile_binary("avl") == 0
 
-    def test_avl(self, clean):
-        """Recompiles AVL tree binary"""
-        assert compile_binary("avl") == 0
-
-    def test_linpack(self, clean):
-        """Recompiles AVL tree binary"""
-        assert compile_binary("linpack") == 0
+def test_linpack(clean):
+    """Recompiles AVL tree binary"""
+    assert compile_binary("linpack") == 0

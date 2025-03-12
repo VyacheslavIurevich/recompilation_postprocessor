2a6e1b65db59018baa17e8997766580967dc23a2
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-03-12 13:14:21 +0300

    chores: automatize mkdir in tests, change gcc to gcc-13
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- src/tests/user_tests.py
+++ src/tests/user_tests.py
@@ -6,12 +6,14 @@ from src.scripts.main import export_c_code
 
 INPUT_DIRECTORY = "res/in/"
 OUTPUT_PATH = "res/out/code.c"
-COMPILER = "gcc"
+COMPILER = "gcc-13"
 
 
 @pytest.fixture(scope='module')
-def clean():
-    """Deleting a.out and code.c files"""
+def chores():
+    """Create output directory before running tests
+    deleting a.out and code.c files after running tests"""
+    os.makedirs("res/out", exist_ok = True)
     yield
     os.remove("a.out")
     os.remove(OUTPUT_PATH)
@@ -24,27 +26,27 @@ def compile_binary(binary):
     return os.system(f"{COMPILER} {OUTPUT_PATH}")
 
 
-def test_hello_world(clean):
+def test_hello_world(chores):
     """Recompiles hello world binary"""
     assert compile_binary("hello_world") == 0
 
-def test_bmp(clean):
+def test_bmp(chores):
     """Recompiles BMP header reader binary"""
     assert compile_binary("bmp1") == 0
 
-def test_bst(clean):
+def test_bst(chores):
     """Recompiles binary search tree binary"""
     assert compile_binary("bst") == 0
 
-def test_avl(clean):
+def test_avl(chores):
     """Recompiles AVL tree binary"""
     assert compile_binary("avl") == 0
 
-def test_linpack(clean):
-    """Recompiles AVL tree binary"""
+def test_linpack(chores):
+    """Recompiles linpack binary"""
     assert compile_binary("linpack") == 0
 
-def test_export_c_code():
+def test_export_c_code(chores):
     """Postprocessor test"""
     for binary in os.listdir(INPUT_DIRECTORY):
         export_c_code(f"{INPUT_DIRECTORY}{binary}", OUTPUT_PATH)
